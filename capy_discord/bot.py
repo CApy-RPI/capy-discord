a3c63c4249b72e266fb6ce063e1143ac8606b14e
Jason Zhang <jasonyue10@gmail.com>
2025-12-16 17:55:16 -0500

    [Bot] add load ext

--- capy_discord/bot.py
+++ capy_discord/bot.py
@@ -2,8 +2,19 @@
 
 from discord.ext.commands import AutoShardedBot
 
+from capy_discord.utils.extensions import EXTENSIONS
+
 
 class Bot(AutoShardedBot):
     """Bot class for Capy Discord."""
+
+    async def setup_hook(self) -> None:
+        """Run before the bot starts."""
+        await self.load_extensions()
+
+    async def load_extensions(self) -> None:
+        """Load all enabled extensions."""
+        for extension in EXTENSIONS:
+            await self.load_extension(extension)
 
     pass
